2fe2edc11872300a84f0833f31d19aa7ff4f0938
Kent Gulka <kentgulka@modindsoft.com>
2022-04-05 20:14:25 -0600

    Mostly completed exercise 7 - missing store name from employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,42 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+# require 'byebug'
+
 puts "Exercise 7"
 puts "----------"
 
+
+#1 Add validations to two models to enforce the following business rules:
+#Employees must always have a first name present
+#Employees must always have a last name present
+
+#Employees have a hourly_rate that is a number (integer) between 40 and 200
+#Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+#2  Ask the user for a store name (store it in a variable)
+
+#3 Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+#4 Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
 # Your code goes here ...
+
+@employees = Employee.where('hourly_rate > ? or hourly_rate < ?',  40, 200)
+#Store.joins(:employees)
+@employees.each do |employee|
+  puts "#{employee.first_name} hourly rate: #{employee.hourly_rate}"
+  puts "employee store? #{employee.stores}"
+end
+####Get the store name from the employee.
+#@storename = Store.find( store_id , :include => [:employee] )
+# puts @store1
+
+store3 = Store.create(name: "Calgary")
+
+store3.errors.full_messages.each do |message|
+  puts message
+end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
+  belongs_to :stores
+  validates :first_name, :last_name, :store_id, presence: true
+
+  validates :store_id, presence: true
+
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
 
 end
+
